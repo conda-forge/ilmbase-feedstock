49382287f77494d20c9a6710ec205db944a2be73
Sylvain Corlay <sylvain.corlay@gmail.com>
2018-11-22 21:27:59 +0100

    Fixup windows build and add run_exports section

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,8 +1,11 @@
-cmake -G "Ninja" ^ 
-      -DCMAKE_PREFIX_PATH=%PREFIX% ^
-      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-      -DCMAKE_FIND_ROOT_PATH="%LIBRARY_PREFIX%" ^
-      -DCMAKE_BUILD_TYPE=Release
+mkdir build
+cd build
 
-ninja
-ninja install
+cmake -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -DCMAKE_FIND_ROOT_PATH="%LIBRARY_PREFIX%" ^
+      -DOPENEXR_BUILD_SHARED=ON ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      -G "NMake Makefiles" ..
+
+nmake
+nmake install
