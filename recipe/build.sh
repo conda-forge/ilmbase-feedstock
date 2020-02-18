c5820b8b073594ec9dd97539b3cb3f027dd72bc4
Wolf Vollprecht <w.vollprecht@gmail.com>
2020-02-18 19:51:29 +0100

    fix up build on unix

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,12 +4,11 @@ cd IlmBase
 mkdir build
 cd build
 
-cmake -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX \
-      -DCMAKE_FIND_ROOT_PATH=$CONDA_PREFIX \
-      -DBUILD_ILMBASE_STATIC=FALSE \
-      -DOPENEXR_BUILD_SHARED=ON \
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_PREFIX_PATH=$PREFIX \
+      -DCMAKE_INSTALL_LIBDIR=lib \
       -DCMAKE_BUILD_TYPE=Release \
       ..
 
-make -j ${CPU_COUNT}
+make -j${CPU_COUNT}
 make install
