f072d06efb9b85fa6eb4a97d0d56a9ab58d43f8e
Wolf Vollprecht <w.vollprecht@gmail.com>
2020-02-21 08:14:39 +0100

    windows support + bump

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,10 +1,11 @@
+cd IlmBase
 mkdir build
 cd build
 
 cmake -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -DCMAKE_FIND_ROOT_PATH="%LIBRARY_PREFIX%" ^
-      -DBUILD_ILMBASE_STATIC=ON ^
-      -DOPENEXR_BUILD_SHARED=ON ^
+      -DBUILD_SHARED_LIBS=ON ^
+      -DILMBASE_LIB_SUFFIX="" ^
       -DCMAKE_BUILD_TYPE=Release ^
       -G "NMake Makefiles" ..
 
