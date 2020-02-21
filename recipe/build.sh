b2bfafd9ab65926584a84e41363ed72d27646e6e
Wolf Vollprecht <w.vollprecht@gmail.com>
2020-02-21 11:10:09 +0100

    ..

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,8 @@ cd build
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DBUILD_SHARED_LIBS=ON \
       -DCMAKE_BUILD_TYPE=Release \
+      -DILMBASE_LIB_SUFFIX="" \
+      -DCMAKE_INSTALL_LIBDIR=lib \
       ..
 
 make -j${CPU_COUNT}
